3a0910ae7eb958d00b6215c66b872e81ecb729c1
Lenardo <leonardovicentin@outlook.com>
2021-02-08 18:32:29 -0300

    ok

--- menu-mobile.js
+++ menu-mobile.js
@@ -7,15 +7,15 @@ const btnMenuClose = document.querySelector('.menu__hamburguer-fechar')
 btnMenu.addEventListener('click', function(){
     menu.classList.add('menu__open');
     
+    
 })
 
 btnMenuClose.addEventListener('click', function(){
     menu.classList.remove('menu__open')
     
-    
-    
 })
-
+    
+    
 menu.addEventListener('click', function(){
     menu.classList.remove('menu__open')
 })
