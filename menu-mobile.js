5175509c87d166eb63ad0a57f8e2af478867fd73
Lenardo <leonardovicentin@outlook.com>
2021-02-08 18:07:02 -0300

    adicionando menu mobile

---
+++ menu-mobile.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.componentes')
+const btnMenu = document.querySelector('.menu__hamburger');
+const btnMenuClose = document.querySelector('.menu__hamburguer-fechar')
+
+
+
+btnMenu.addEventListener('click', function(){
+    menu.classList.add('menu__open');
+    
+})
+
+btnMenuClose.addEventListener('click', function(){
+    menu.classList.remove('menu__open')
+    
+    
+    
+})
